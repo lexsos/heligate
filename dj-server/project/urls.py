8a17c90fe3c06024ac6ad2f579ac1fa81eafa224
lexsos <ale-sosnin@yandex.ru>
2014-05-13 15:26:23 +0400

    Add django skeleton

---
+++ dj-server/project/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin_tools/', include('admin_tools.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
