3855e4e05e7ba9ff491f83a858ecb6b54b77ca2f
lexsos <ale-sosnin@yandex.ru>
2014-05-20 10:25:50 +0400

    Fix code style

--- dj-server/apps/firewall/utils.py
+++ dj-server/apps/firewall/utils.py
@@ -27,9 +27,9 @@ def get_ipt_params(classifier):
 
 def normalize_script(script):
     norma = script
-    norma= re.sub(u' +', u' ', norma)
-    norma= re.sub(u'\n\s*', u'\n', norma)
-    norma= re.sub(u'^\s*\n', u'', norma)
+    norma = re.sub(u' +', u' ', norma)
+    norma = re.sub(u'\n\s*', u'\n', norma)
+    norma = re.sub(u'^\s*\n', u'', norma)
     return norma
 
 
