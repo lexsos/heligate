ea2e126f27b5dca83e65ab7f471c5d6fb5cc0199
lexsos <ale-sosnin@yandex.ru>
2014-06-04 14:15:36 +0400

    Add events_clear command

---
+++ dj-server/apps/event_log/management/commands/events_clear.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+
+from event_log.models import Event
+
+
+class Command(BaseCommand):
+    help = 'Delete all event log entries'
+
+    def handle(self, *args, **options):
+
+        Event.objects.all().delete()
+        self.stdout.write('All entries successfully deleted')
--- dj-server/rc.d/start.sh
+++ dj-server/rc.d/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ../manage.py accounts_clear_entries
+../manage.py events_clear
 ../manage.py reg_static_ip
 ../manage.py firewall_gen_config | /bin/sh
