7238ed88f6cf79b0d5610e36ff3892454f4b368c
lexsos <ale-sosnin@yandex.ru>
2014-06-16 12:41:50 +0400

    Add squid loger cache parameters

--- dj-server/apps/squid3/loger.py
+++ dj-server/apps/squid3/loger.py
@@ -2,8 +2,11 @@ import re
 
 from accounts.utils import get_user_by_ip4
 from .models import Domain, SquidLog
+from .settings import CONFIG
 
 
+DOMAIN_CACHE_SIZE = CONFIG['DOMAIN_CACHE_SIZE']
+COUNTER_MUL = CONFIG['COUNTER_MUL']
 re_url = re.compile(r'^https?://(?P<domain>[^ \f\n\r\t\v/]+)')
 
 
@@ -31,6 +34,9 @@ class SquidLoger(object):
 
         return user
 
+    def clear_user_cache(self):
+        self.users = {}
+
     def get_domain(self, domain_name):
 
         domain = self.domains_l1.get(domain_name)
@@ -44,8 +50,8 @@ class SquidLoger(object):
             domain, created = Domain.objects.get_or_create(dns=domain_name)
             self.domains_l1[domain_name] = domain
 
-        if self.domains_counter > 1000*2:
-            if len(self.domains_l1) > 1000:
+        if self.domains_counter > DOMAIN_CACHE_SIZE*COUNTER_MUL:
+            if len(self.domains_l1) > DOMAIN_CACHE_SIZE:
                 self.domains_l2 = self.domains_l1
                 self.domains_l1 = {}
                 self.domains_counter = 0
@@ -65,6 +71,8 @@ class SquidLoger(object):
         user_ip, size, url = self.pars_squid_data(squid_string)
         if url:
             domain_name = extruct_domain(url).lower()
+            if domain_name is None:
+                domain_name = 'INVALID DOMAIN'
             domain = self.get_domain(domain_name)
             user = self.get_user(user_ip)
             rec = SquidLog(
--- dj-server/apps/squid3/settings.py
+++ dj-server/apps/squid3/settings.py
@@ -4,6 +4,8 @@ from django.conf import settings
 CONFIG = {
     'HTTP_PORT': 3129,
     'HTTPS_PORT': 3128,
+    'DOMAIN_CACHE_SIZE': 1000,
+    'COUNTER_MUL': 2,
 }
 
-CONFIG.update(getattr(settings, 'ACCOUNTS_DYNAMIC_IP_CONFIG', {}))
+CONFIG.update(getattr(settings, 'SQUID3_CONFIG', {}))
